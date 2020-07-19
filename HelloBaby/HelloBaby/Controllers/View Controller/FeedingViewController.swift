347f27bb135c6da47b370a11c11aafb21721042d
Kristin Samuels <krislanza@yahoo.com>
2020-07-19 16:54:51 -0400

    Clear out old implementations from FeedingViewController

--- HelloBaby/HelloBaby/Controllers/View Controller/FeedingViewController.swift
+++ HelloBaby/HelloBaby/Controllers/View Controller/FeedingViewController.swift
@@ -3,69 +3,7 @@ import UIKit
 
 class FeedingViewController: UIViewController {
     
-    @IBOutlet weak var addFeedingButton: UIButton!
-    @IBOutlet weak var volumeSlider: UISlider!
-    @IBOutlet weak var volumeLabel: UILabel!
     @IBOutlet weak var feedingTypeSegmentedControl: UISegmentedControl!
-    
-    @IBOutlet weak var minuteLabel: UILabel!
-    
-    @IBOutlet weak var secondLabel: UILabel!
-    
-    
-    var isBottleFeeding: Bool = true
-    var minutes = 0
-    var seconds = 0
-    var timer = NursingTimer()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        //        let shapeLayer = CAShapeLayer()
-        //
-        //        let center = view.center
-        //        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        //        shapeLayer.path = circularPath.cgPath
-        //
-        //        shapeLayer.strokeColor = UIColor.systemPink.cgColor
-        //        shapeLayer.lineWidth = 10
-        //
-        //
-        //        view.layer.addSublayer(shapeLayer)
-        //
-        //        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super .viewWillAppear(animated)
-    }
-    //Mark: Nursing 
-    
-    @IBAction func leftSideButtonTapped(_ sender: UIButton) {
-    }
-    
-    
-    @IBAction func rightSideButtonTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func startButtonTapped(_ sender: UIButton) {
-        if timer.isActive {
-            timer.stopTimer()
-        }
-        
-    }
-    
-    // Mark- Bottle Feeding
-    @IBAction func volumeSliderMoved(_ sender: UISlider) {
-        volumeLabel.text = String((sender.value * 20).rounded()/2)
-    }
-    
-    @IBAction func finishFeedingButtonTapped(_ sender: UIButton) {
-        guard let volume = volumeLabel.text else {fatalError()}
-        guard let newVolume = Double(volume) else {fatalError()}
-        FeedingController.shared.createFeeding(volume: newVolume, date: Date(), isBottleFeeding: isBottleFeeding, duration: 0.0)
-    }
     
     @IBOutlet weak var nursingContainerView: UIView!
     @IBOutlet weak var bottleContainerView: UIView!
@@ -85,18 +23,4 @@ class FeedingViewController: UIViewController {
             }
         }
     }
-    //switch sender.selectedSegmentIndex {
-    //        case 0:
-    //            self.feedingType = .nursing
-    //        case 1:
-    //            self.feedingType = .bottleFed
-    //        default:
-    //            break
-    //        }
-    
 }
-
-//extension FeedingViewController: UITableViewDelegate, UITableViewDataSource {
-//
-//
-//}
