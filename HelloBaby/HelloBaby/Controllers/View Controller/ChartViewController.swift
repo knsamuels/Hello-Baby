895c62c0778f61b1623d17d6848368da96492004
Kristin Samuels <krislanza@yahoo.com>
2020-07-19 22:51:34 -0400

    Add Charts framework via swift package manager

---
+++ HelloBaby/HelloBaby/Controllers/View Controller/ChartViewController.swift
@@ -0,0 +1,11 @@
+import Charts
+import UIKit
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var barChart: BarChartView!
+}
